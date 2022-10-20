ae5209db3e220cd019af8f2ecf879af2e1634b22
Samuel JS Wright <swrig109@uncc.edu>
2022-10-20 10:04:22 -0400

    edit

--- traffic_stops_app.py
+++ traffic_stops_app.py
@@ -5,13 +5,14 @@ Created on Mon Oct 20 09:01:22 2022
 
 @author: Samsonite
 """
+#%%
 from functools import cache
 from multiprocessing.resource_sharer import stop
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+#%%
 @st.cache
 def open_clean_file():
     url = "https://drive.google.com/file/d/1P9wUxzlFcXs_sC0jBGBlcdyP56OMvK7W/view?usp=sharing"
@@ -29,3 +30,5 @@ def open_clean_file():
     return stops_filt
 
 stops_filt = open_clean_file()
+st.write("View of the first few rows!")
+st.table(stops_filt.head())
