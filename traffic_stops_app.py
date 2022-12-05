766c31ee30799a7d54b910949bc4db5c9e6e1d07
Samuel JS Wright <swrig109@uncc.edu>
2022-12-05 09:05:02 -0500

    code styling

--- traffic_stops_app.py
+++ traffic_stops_app.py
@@ -11,52 +11,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from sklearn.tree import DecisionTreeClassifier #DT classifier
-from sklearn.preprocessing import OneHotEncoder #label encoding for naive bayes
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
 
 
 # %%
 st.title("Visualization and predictive modeling of traffic stop data for the City of Charlotte")
 st.markdown("Data sourced from [City of Charlotte Open Data Portal](https://data.charlottenc.gov/datasets/charlotte::officer-traffic-stops/explore)")
 st.write("The data for this project was pulled 10/17/2022")
+
+
 @st.cache
 def open_clean_file():
     url = "https://drive.google.com/file/d/1P9wUxzlFcXs_sC0jBGBlcdyP56OMvK7W/view?usp=sharing"
-    path = 'https://drive.google.com/uc?export=download&id='+\
-            url.split('/')[-2]
+    path = 'https://drive.google.com/uc?export=download&id=' +\
+           url.split('/')[-2]
     stops = pd.read_csv(path)
-    stops.drop(columns = ["GlobalID", "OBJECTID"], inplace=True)
+    stops.drop(columns=["GlobalID", "OBJECTID"], inplace=True)
     stops["Month_of_Stop"] = pd.to_datetime(stops.Month_of_Stop)
     stops_ob = stops.select_dtypes(["object"])
     stops[stops_ob.columns] = stops_ob.apply(lambda x: x.str.strip())
-    stops_filt = stops[~ stops.Officer_Race.isin(["2 or More",\
+    stops_filt = stops[~ stops.Officer_Race.isin(["2 or More",
                        "Not Specified"])].copy()
-    stops_filt.drop(stops_filt[stops_filt.Driver_Race ==\
-                               "Other/Unknown"].index, inplace = True)
-    stops_filt.drop(stops_filt[stops_filt.Reason_for_Stop ==\
+    stops_filt.drop(stops_filt[stops_filt.Driver_Race ==
+                               "Other/Unknown"].index, inplace=True)
+    stops_filt.drop(stops_filt[stops_filt.Reason_for_Stop ==
                                "Other"].index, inplace=True)
     stops_filt.dropna(inplace=True)
-    stops_filt["Driver_Age"] = pd.cut(stops_filt.Driver_Age,\
-                                      bins = [0,25,35,55,200],
-                                      labels = ["Under 25",
-                                                "25 to 34",
-                                                "35 to 54",
-                                                "55 and older"])
+    stops_filt["Driver_Age"] = pd.cut(stops_filt.Driver_Age,
+                                      bins=[0, 25, 35, 55, 200],
+                                      labels=["Under 25",
+                                              "25 to 34",
+                                              "35 to 54",
+                                              "55 and older"])
     stops_filt["Officer_Years_of_Service"] =\
-               pd.cut(stops_filt.Officer_Years_of_Service,\
-                      bins = [0,5,10,15,100],\
-                      labels = ["Less than 5",
-                                "5 to 9",
-                                "10 to 15",
-                                "Over 15"])
+        pd.cut(stops_filt.Officer_Years_of_Service,
+               bins=[0, 5, 10, 15, 100],
+               labels=["Less than 5",
+                       "5 to 9",
+                       "10 to 15",
+                       "Over 15"])
     return stops_filt
+
 
 stops_filt = open_clean_file()
 st.write("View of the first few rows!")
 with st.expander("Show first 5 rows of the dataframe"):
     st.table(stops_filt.head())
 
-optionsb = [c for c in stops_filt.columns if not c in ['Month_of_Stop']]
+optionsb = [c for c in stops_filt.columns if c not in ['Month_of_Stop']]
 
 targetgrp = st.selectbox(
     "Variable to build histogram from",
@@ -66,14 +69,14 @@ st.write("You chose: ", targetgrp)
 
 figa, axa = plt.subplots()
 sns.histplot(data=stops_filt, y=targetgrp, ax=axa)
-axa.set_title("Histogram of traffic stops by \"{0}\"\nin Charlotte, NC".\
-              format(targetgrp.replace("_"," ")))
-axa.set_ylabel(targetgrp.replace("_"," "))
+axa.set_title("Histogram of traffic stops by \"{0}\"\nin Charlotte, NC".
+              format(targetgrp.replace("_", " ")))
+axa.set_ylabel(targetgrp.replace("_", " "))
 
 with st.expander("Show histogram: "):
     st.pyplot(figa)
 
-optionsc = [c for c in stops_filt.columns if not c in ["Month_of_Stop"]]
+optionsc = [c for c in stops_filt.columns if c not in ["Month_of_Stop"]]
 
 choiceca = st.selectbox(
     "Which variable would you like to plot?",
@@ -84,10 +87,10 @@ choicecb = st.selectbox(
     optionsc
 )
 
-figc,axc = plt.subplots()
+figc, axc = plt.subplots()
 sns.countplot(data=stops_filt, y=choiceca, hue=choicecb, ax=axc)
-axc.set_title("Histogram of {} grouped by {}".\
-              format(choiceca.replace("_"," "),choicecb.replace("_"," ")))
+axc.set_title("Histogram of {} grouped by {}".
+              format(choiceca.replace("_", " "), choicecb.replace("_", " ")))
 axc.set_ylabel(choiceca.replace("_", " "))
 st.pyplot(figc)
 
@@ -102,8 +105,8 @@ def build_model():
     min_stops["Target"] = np.where(min_stops.Target == "Arrest", 1, 0)
     count_class_0, _ = min_stops.Target.value_counts()
     stops_arrest = min_stops[min_stops.Target == 1]
-    stops_arrest_over = stops_arrest.sample(int(np.round(count_class_0 *.5)),\
-                                            replace = True)
+    stops_arrest_over = stops_arrest.sample(int(np.round(count_class_0 * .5)),
+                                            replace=True)
     min_stops = pd.concat([min_stops, stops_arrest_over], axis=0)
     dt_X = min_stops.drop(columns="Target")
     dt_y = min_stops["Target"]
@@ -115,7 +118,11 @@ def build_model():
     model.fit(dt_enc_X, dt_y)
     conditions = [list(min_stops[col].unique()) for col in min_stops]
     return model, ohedt, conditions
+
+
 DT_model, encoder, inputs = build_model()
+
+
 # %%
 st.markdown("**Predictive Modeling:**")
 st.write("Choose the parameters of interest you would like to model")
