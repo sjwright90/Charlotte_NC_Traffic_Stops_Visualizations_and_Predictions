46ec861bf537fde1431343443ec2779aae16211b
Samuel JS Wright <swrig109@uncc.edu>
2022-12-05 10:35:36 -0500

    rename homepage

--- 🌐_Home_Page.py
+++ 🌐_Home_Page.py
@@ -20,9 +20,12 @@ st.title("Visualization and predictive modeling of traffic stop data for the Cit
 st.markdown("Data sourced from [City of Charlotte Open Data Portal](https://data.charlottenc.gov/datasets/charlotte::officer-traffic-stops/explore)")
 st.write("The data for this project was pulled 10/17/2022")
 
-
 with st.spinner("Getting your data ready"):
     stops_filt = open_clean_file()
+st.subheader("Understanding policing data in the Charlotte, NC metropolitan area")
+st.markdown('''
+
+''')
 st.write("View of the first few rows!")
 with st.expander("Show first 5 rows of the dataframe"):
     st.table(stops_filt.head())
